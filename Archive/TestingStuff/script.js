5fac3d0569258e0406f51a89362a93d3b4ba409f
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2026-01-19 18:14:10 -0500

    largedir

---
+++ Archive/TestingStuff/script.js
@@ -0,0 +1,24 @@
+var portfolio = [
+    {
+        category: 'Example',
+        projects: [
+            {
+                title: "Title1",
+                description: "text1",
+                image: "arcwallpaper.png",
+                link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            }
+        ]
+    },
+    {
+        category: 'Example2',
+        projects: [
+            {
+                title: "Title2",
+                description: "text2",
+                image: "arcwallpaper.png",
+                link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            }
+        ]
+    },
+];
